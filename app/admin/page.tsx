0426ef817981c1504f95ae2af329f143b9bf4ec1
GSGFs <gsgfs@gsgfs.moe>
2025-02-23 14:43:31 +0800

    remove heroui

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,7 @@
-import TestButton from "@/components/test-button";
-import { generateSitemap } from "@/utils/sitemap";
-
 export default async function Page() {
   return (
     <>
       <div>Admin</div>
-      <TestButton action={generateSitemap} />
     </>
   );
 }
