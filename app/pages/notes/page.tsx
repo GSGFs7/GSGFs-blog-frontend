fd25f69b5d2f776ba9a8872b53c716ca1e4eb026
GSGFs <gsgfs@gsgfs.moe>
2025-03-23 15:13:59 +0800

    fix(notes): add placeholder text to fix not configured edge runtime error

--- app/pages/notes/page.tsx
+++ app/pages/notes/page.tsx
@@ -1,5 +1,11 @@
 export const runtime = "edge";
 
 export default async function Page() {
-  return <div></div>;
+  return (
+    <div>
+      <p>杂鱼~</p>
+      <p>这里还没有写完哦</p>
+      <p>晚点再来看看吧</p>
+    </div>
+  );
 }
