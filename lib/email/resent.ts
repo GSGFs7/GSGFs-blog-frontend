f96113dc15d56fbca80f5ad9b5d0223798f542db
GSGFs <gsgfs@gsgfs.moe>
2025-04-17 23:08:54 +0800

    fix(email): move Resend initialization inside mailAdmin function

--- lib/email/resent.ts
+++ lib/email/resent.ts
@@ -2,12 +2,11 @@ import { CreateEmailResponse, Resend } from "resend";
 
 import { EmailTemplate } from "./email-template";
 
-const resend = new Resend(process.env.RESEND_API_KEY ?? "");
-
 export async function mailAdmin(
   subject: string,
   content: string,
 ): Promise<CreateEmailResponse> {
+  const resend = new Resend(process.env.RESEND_API_KEY ?? "");
   const response = await resend.emails.send({
     from: process.env.DEFAULT_FROM_EMAIL!,
     to: process.env.ADMIN_EMAIL!,
