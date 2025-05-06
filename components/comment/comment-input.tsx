4c0e437ab43c4b35b5da31b932ca9f442bd20e6b
GSGFs <gsgfs@gsgfs.moe>
2025-05-06 21:39:04 +0800

    chore(comment): always render TurnstileWidget regardless of disabled state

--- components/comment/comment-input.tsx
+++ components/comment/comment-input.tsx
@@ -116,7 +116,7 @@ export default function CommentInput({
         onChange={(e) => saveDraft(e.target.value)}
       />
 
-      {!disabled && <TurnstileWidget setToken={setTurnstileToken} />}
+      <TurnstileWidget setToken={setTurnstileToken} />
     </form>
   );
 }
