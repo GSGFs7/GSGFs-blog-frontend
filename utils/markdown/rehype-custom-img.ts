0e84445f6c9843296989266d57e60891d94015cb
GSGFs <gsgfs@gsgfs.moe>
2025-05-17 20:26:12 +0800

    feat(blog): use '/_next/image' optimize image in blog markdown

--- utils/markdown/rehype-custom-img.ts
+++ utils/markdown/rehype-custom-img.ts
@@ -1,7 +1,9 @@
 import { visit, SKIP } from "unist-util-visit";
 import { Element } from "hast";
 
-export default function rehypeCustomImg() {
+export default function rehypeCustomImg(
+  options = { width: 1080, quality: 90 },
+) {
   return (tree: any) => {
     visit(
       tree,
@@ -9,6 +11,14 @@ export default function rehypeCustomImg() {
       (node: Element, index: number | undefined, parent: any) => {
         if (node.tagName === "img") {
           const alt = node.properties?.alt?.toString() || "";
+          const src = node.properties?.src?.toString() || "";
+
+          if (src && !src.startsWith("/")) {
+            const encodedSrc = encodeURIComponent(src);
+            const optimizedSrc = `/_next/image?url=${encodedSrc}&w=${options.width}&q=${options.quality}`;
+
+            node.properties.src = optimizedSrc;
+          }
 
           const wrapper = {
             type: "element",
@@ -16,6 +26,7 @@ export default function rehypeCustomImg() {
             properties: {
               className: ["image-wrapper"],
               dataAlt: alt,
+              dataSrc: src,
             },
             children: [{ ...node }],
           };
