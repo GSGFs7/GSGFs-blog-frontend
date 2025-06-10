e3e797c632f0db0655ea1fd1627482bd99cfcc8e
GSGFs <gsgfs@gsgfs.moe>
2025-06-10 21:42:44 +0800

    test: add cache test

--- app/strange/page.tsx
+++ app/strange/page.tsx
@@ -1,9 +1,19 @@
+import TestButton from "@/components/test-button";
+import { cacheGet } from "@/lib/cache";
+
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
+  const action = async () => {
+    "use server";
+    // eslint-disable-next-line no-console
+    console.log(await cacheGet("114514"));
+  };
+
   return (
     <div>
       <p>A strange page</p>
+      <TestButton action={action} />
     </div>
   );
 }
--- components/test-button.tsx
+++ components/test-button.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-//! 仅测试使用
 export default function TestButton({ action }: { action: () => void }) {
   return (
     <form action={action}>
