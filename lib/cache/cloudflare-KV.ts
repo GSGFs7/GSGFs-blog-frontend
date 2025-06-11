8190f8829b882828f2f2e0a8901aa04743737f1b
GSGFs <gsgfs@gsgfs.moe>
2025-06-11 20:58:59 +0800

    fix: cannot find module 'cloudflare:workers'

--- lib/cache/cloudflare-KV.ts
+++ lib/cache/cloudflare-KV.ts
@@ -1,72 +1,72 @@
-// have some problem
-import { env } from "cloudflare:workers";
+// // have some problem
+// import { env } from "cloudflare:workers";
 
-import { CacheTTL } from ".";
+// import { CacheTTL } from ".";
 
-import { CacheError, CacheInterface } from "@/types/cache";
+// import { CacheError, CacheInterface } from "@/types/cache";
 
-function createCacheError(
-  operation: CacheError["operation"],
-  key: string | undefined,
-  originalError: unknown,
-): CacheError {
-  const error = new Error(
-    `Cache ${operation} operation failed${key ? ` for key '${key}'` : ""}`,
-  ) as CacheError;
+// function createCacheError(
+//   operation: CacheError["operation"],
+//   key: string | undefined,
+//   originalError: unknown,
+// ): CacheError {
+//   const error = new Error(
+//     `Cache ${operation} operation failed${key ? ` for key '${key}'` : ""}`,
+//   ) as CacheError;
 
-  error.operation = operation;
-  error.key = key;
-  error.originalError = originalError;
+//   error.operation = operation;
+//   error.key = key;
+//   error.originalError = originalError;
 
-  return error;
-}
+//   return error;
+// }
 
-export async function cacheGet<T>(key: string): Promise<T | null> {
-  try {
-    const res = await env.NEXT_INC_CACHE_KV.get<T>(key, { type: "json" });
+// export async function cacheGet<T>(key: string): Promise<T | null> {
+//   try {
+//     const res = await env.NEXT_INC_CACHE_KV.get<T>(key, { type: "json" });
 
-    return res;
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(`Get '${key}' cache failed: `, e);
-    throw createCacheError("get", key, e);
-  }
-}
+//     return res;
+//   } catch (e) {
+//     // eslint-disable-next-line no-console
+//     console.error(`Get '${key}' cache failed: `, e);
+//     throw createCacheError("get", key, e);
+//   }
+// }
 
-export async function cacheSet<T>(
-  key: string,
-  value: T,
-  ttlSeconds: number = CacheTTL,
-): Promise<boolean> {
-  try {
-    await env.NEXT_INC_CACHE_KV.put(key, JSON.stringify(value), {
-      expirationTtl: ttlSeconds,
-    });
+// export async function cacheSet<T>(
+//   key: string,
+//   value: T,
+//   ttlSeconds: number = CacheTTL,
+// ): Promise<boolean> {
+//   try {
+//     await env.NEXT_INC_CACHE_KV.put(key, JSON.stringify(value), {
+//       expirationTtl: ttlSeconds,
+//     });
 
-    return true;
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(`Set '${key}' cache failed: `, e);
-    throw createCacheError("set", key, e);
-  }
-}
+//     return true;
+//   } catch (e) {
+//     // eslint-disable-next-line no-console
+//     console.error(`Set '${key}' cache failed: `, e);
+//     throw createCacheError("set", key, e);
+//   }
+// }
 
-export async function cacheDelete(key: string): Promise<boolean> {
-  try {
-    await env.NEXT_INC_CACHE_KV.delete(key);
+// export async function cacheDelete(key: string): Promise<boolean> {
+//   try {
+//     await env.NEXT_INC_CACHE_KV.delete(key);
 
-    return true;
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.error(`Delete '${key}' cache failed: `, e);
-    throw createCacheError("delete", key, e);
-  }
-}
+//     return true;
+//   } catch (e) {
+//     // eslint-disable-next-line no-console
+//     console.error(`Delete '${key}' cache failed: `, e);
+//     throw createCacheError("delete", key, e);
+//   }
+// }
 
-const cloudflareKVCache: CacheInterface = {
-  cacheGet,
-  cacheSet,
-  cacheDelete,
-};
+// const cloudflareKVCache: CacheInterface = {
+//   cacheGet,
+//   cacheSet,
+//   cacheDelete,
+// };
 
-export default cloudflareKVCache;
+// export default cloudflareKVCache;
