a686271352b1b6809cab3566d80c64f0d952c9ee
GSGFs <gsgfs@gsgfs.moe>
2025-07-06 17:38:12 +0800

    style: adjust padding in Home component layout

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   const gpg = await commentMarkdownToHtml(GPGMd);
 
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+    <section className="flex flex-col items-center justify-center gap-4 py-4 md:py-6">
       <First gpg={gpg} />
     </section>
   );
