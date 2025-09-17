e098d204887a67373e925787cb78a1ca1a634610
GSGFs <gsgfs@gsgfs.moe>
2025-09-17 21:34:55 +0800

    feat: use 'content_update_at' replace 'update_at' in blog page

--- app/strange/page.tsx
+++ app/strange/page.tsx
@@ -1,21 +1,12 @@
 import CacheStatus from "@/components/cache-status";
-import TestButton from "@/components/test-button";
-import { getPostBySearch } from "@/lib/api";
 
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-  const action = async () => {
-    "use server";
-    const res = await getPostBySearch("python");
-    console.log(res);
-  };
-
   return (
     <div>
       <p>A strange page</p>
       <CacheStatus />
-      <TestButton action={action} />
     </div>
   );
 }
