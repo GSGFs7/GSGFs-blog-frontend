35ca3c590f37b6c10441e4ba15e60291cfdd0cf2
GSGFs <gsgfs@gsgfs.moe>
2025-10-08 20:53:10 +0800

    refactor: refactor comment remove unnecessary props

--- components/comment/button.tsx
+++ components/comment/button.tsx
@@ -1,7 +1,20 @@
-export default function Button({ disabled = false }: { disabled?: boolean }) {
+"use client";
+
+import clsx from "clsx";
+
+import { useAuth } from "@/app/providers";
+
+export default function Button() {
+  const { session } = useAuth();
+
   return (
     <button
-      className={`mr-2 rounded-lg bg-blue-600 px-4 py-2 sm:px-5 ${disabled ? "cursor-not-allowed bg-blue-800 text-gray-400" : "cursor-pointer"}`}
+      className={clsx(
+        `mr-2 rounded-lg bg-blue-600 px-4 py-2 sm:px-5`,
+        session
+          ? "cursor-pointer"
+          : "cursor-not-allowed bg-blue-800 text-gray-400",
+      )}
       form="comment-form"
       type="submit"
     >
