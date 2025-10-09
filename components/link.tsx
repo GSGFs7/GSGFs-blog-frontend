2637da7e6a7ad05b7237c0526808f4c4a138b8c3
GSGFs <gsgfs@gsgfs.moe>
2025-10-09 16:02:51 +0800

    feat: add loading context and custom link component to manage global page loading state

---
+++ components/link.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { LinkProps, default as NextLink } from "next/link";
+import React from "react";
+
+import { useLoading } from "@/app/providers";
+
+export const Link = React.forwardRef<
+  HTMLAnchorElement,
+  Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps<any>> &
+    LinkProps<any>
+>(({ href, children, onNavigate, ...rest }, ref) => {
+  const { setIsLoading } = useLoading();
+  return (
+    <NextLink
+      href={href}
+      onNavigate={(event) => {
+        setIsLoading(true);
+
+        if (onNavigate) {
+          onNavigate(event);
+        }
+      }}
+      {...rest}
+      ref={ref}
+    >
+      {children}
+    </NextLink>
+  );
+});
+
+Link.displayName = "Link";
+
+export default Link;
