ea8b1169ada9197a7832f9e9f049d4e7e754800d
GSGFs <gsgfs@gsgfs.moe>
2025-10-09 17:03:02 +0800

    fix: remove loading state from NavLogoFallback component

--- components/navbar/nav-logo-fallback.tsx
+++ components/navbar/nav-logo-fallback.tsx
@@ -1,28 +1,13 @@
-"use client";
+import { LuLollipop } from "react-icons/lu";
 
-import clsx from "clsx";
-import { LuLollipop, LuRefreshCw } from "react-icons/lu";
-
-import { useLoading } from "@/app/providers";
 import { siteConfig } from "@/config/site";
 
 import Link from "../link";
 
 export function NavLogoFallback() {
-  const { isLoading } = useLoading();
-
   return (
     <Link className="flex items-center justify-start gap-1" href="/">
-      {isLoading ? (
-        <LuRefreshCw
-          className={clsx(
-            "relative -translate-y-[0.15rem] text-2xl",
-            "animate-spin",
-          )}
-        />
-      ) : (
-        <LuLollipop className="relative -translate-y-[0.15rem] text-2xl" />
-      )}
+      <LuLollipop className="relative -translate-y-[0.15rem] text-2xl" />
       <p className="inline-block text-lg font-bold">
         <span className="whitespace-nowrap">{siteConfig.siteName}</span>
       </p>
