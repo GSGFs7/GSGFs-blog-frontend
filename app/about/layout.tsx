87ff09a258f2d8461a5d2b04461ba74debad4727
GSGFs <gsgfs@gsgfs.moe>
2025-10-30 13:08:47 +0800

    fix: content overflows in chrome browser

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -15,7 +15,7 @@ export default function AboutLayout({
 }) {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block w-full max-w-3xl justify-center">
+      <div className="inline-block w-fit max-w-3xl justify-center">
         {children}
       </div>
     </section>
