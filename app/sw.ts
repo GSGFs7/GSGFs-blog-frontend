1eabfa5e0524d1c72e1b82336476111257023510
GSGFs <gsgfs@gsgfs.moe>
2025-10-31 15:45:27 +0800

    fix: comment out 'Serwist'

--- app/sw.ts
+++ app/sw.ts
@@ -1,45 +1,45 @@
-import { defaultCache } from "@serwist/next/worker";
-import {
-  PrecacheEntry,
-  RuntimeCaching,
-  Serwist,
-  SerwistGlobalConfig,
-} from "serwist";
+// import { defaultCache } from "@serwist/next/worker";
+// import {
+//   type PrecacheEntry,
+//   type RuntimeCaching,
+//   Serwist,
+//   type SerwistGlobalConfig,
+// } from "serwist";
 
-declare global {
-  interface WorkerGlobalScope extends SerwistGlobalConfig {
-    __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
-  }
-}
+// declare global {
+//   interface WorkerGlobalScope extends SerwistGlobalConfig {
+//     __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
+//   }
+// }
 
-declare const self: ServiceWorkerGlobalScope;
+// declare const self: ServiceWorkerGlobalScope;
 
-// the last rule will disable all cross site request
-// so, we need to change it
-const customCache: RuntimeCaching[] = defaultCache;
-const lastCache = customCache.at(-1);
+// // the last rule will disable all cross site request
+// // so, we need to change it
+// const customCache: RuntimeCaching[] = defaultCache;
+// const lastCache = customCache.at(-1);
 
-if (lastCache) {
-  // allow Turnstile to pass through
-  lastCache.matcher = ({ url, sameOrigin }) => {
-    if (url.origin === "https://challenges.cloudflare.com") {
-      return false;
-    }
+// if (lastCache) {
+//   // allow Turnstile to pass through
+//   lastCache.matcher = ({ url, sameOrigin }) => {
+//     if (url.origin === "https://challenges.cloudflare.com") {
+//       return false;
+//     }
 
-    return !sameOrigin;
-  };
+//     return !sameOrigin;
+//   };
 
-  // use the new rule
-  customCache.pop();
-  customCache.push(lastCache);
-}
+//   // use the new rule
+//   customCache.pop();
+//   customCache.push(lastCache);
+// }
 
-const serwist = new Serwist({
-  precacheEntries: self.__SW_MANIFEST,
-  skipWaiting: true,
-  clientsClaim: true,
-  navigationPreload: true,
-  runtimeCaching: customCache,
-});
+// const serwist = new Serwist({
+//   precacheEntries: self.__SW_MANIFEST,
+//   skipWaiting: true,
+//   clientsClaim: true,
+//   navigationPreload: true,
+//   runtimeCaching: customCache,
+// });
 
-serwist.addEventListeners();
+// serwist.addEventListeners();
