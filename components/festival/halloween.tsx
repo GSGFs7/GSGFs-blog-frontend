9fe45e100565e82aa8458dd86b2c892fd79721d8
GSGFs <gsgfs@gsgfs.moe>
2025-11-03 15:02:30 +0800

    fix: pumpkin component overflow

--- components/festival/halloween.tsx
+++ components/festival/halloween.tsx
@@ -3,7 +3,7 @@ import { GiPumpkinLantern } from "react-icons/gi";
 export function HalloweenPumpkin() {
   return (
     <GiPumpkinLantern
-      className="absolute inset-0 -top-56 left-64 -z-10 h-96 w-96 text-orange-500 opacity-70"
+      className="pointer-events-none absolute inset-0 -top-56 left-64 -z-10 hidden h-96 w-96 text-orange-500 opacity-70 lg:block"
       style={{ transform: "rotate(45deg)" }}
     />
   );
